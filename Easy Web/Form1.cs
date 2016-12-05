9cd0fb752a6093d78407a25065c76bab03c78060
rizwan3d <allgame45@hotmail.com>
2016-12-05 18:54:08 +0500

    3

--- Easy Web/Form1.cs
+++ Easy Web/Form1.cs
@@ -1128,7 +1128,7 @@ namespace Easy_Web
             }
 
             this.WindowState = FormWindowState.Maximized;
-
+            splitContainerControl1.SplitterPosition = this.Width - 330;
         }
 
         private void ContentsItem3_LinkClicked(object sender, NavBarLinkEventArgs e)
